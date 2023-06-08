2646e024ddc08baf5700b0c11b9f21e128640a77
HaHaIHaveAShare <hahaihaveashare@gmail.com>
2023-06-07 21:45:58 -0700

    Vert shader fix
    Forgot to stage the changes for vert.glsl

--- src/vert.glsl
+++ src/vert.glsl
@@ -1,10 +1,10 @@
 #version 330
 
-in vec3 pos;
+in vec3 position;
 in vec3 color;
 out vec3 v_color;
 
 void main () {
   v_color = color;
-  gl_Position = vec4(pos, 1.0);
+  gl_Position = vec4(position, 1.0);
 }
