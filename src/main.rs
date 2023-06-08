ec7c3239b9c7932ac81872c9bb274196d099b119
HaHaIHaveAShare <hahaihaveashare@gmail.com>
2023-06-07 21:22:02 -0700

    Debug, Dedupe, Deobfuscate
    Restructured a lot of the internals for better legibility
    and future abstraction/generalization

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,161 @@
 use glium::Surface;
 
-const JUMP_COOLDOWN: std::time::Duration = std::time::Duration::from_millis(250);
-const ACCEL_GRAV: f32 = -0.9;
-const TERM_VEL: f32 = -1.0;
+const BIRDY_ACCEL_GRAV: f32 = -0.9;
+const BIRDY_ACCEL_JUMP: f32 = 0.8;
+const BIRDY_ACCEL_MOVE: f32 = 0.6;
+const BIRDY_DECCEL_MOVE: f32 = -0.4; // decceleration applied when player isn't holding a direction key
+const BIRDY_JUMP_COOLDOWN: std::time::Duration = std::time::Duration::from_millis(250);
+const BIRDY_TERMINAL_VELOCITY: f32 = -1.0;
+const BIRDY_COLOR: (f32, f32, f32) = (1.0, 0.9, 0.0);
+const BIRDY_DEPTH: f32 = 0.0;
+const BIRDY_SIZE: f32 = 0.1;
+
 const ROCK_COOLDOWN: std::time::Duration = std::time::Duration::from_millis(750);
+const ROCK_DESPAWN_DIST: f32 = 2.0;
+const ROCK_MIN_VELOCITY: f32 = 0.5;
+const ROCK_MAX_VELOCITY: f32 = 1.0;
+const ROCK_SPAWN_DIST: f32 = 1.5;
+const ROCK_COLOR: (f32, f32, f32) = (0.2, 0.2, 0.3);
+const ROCK_DEPTH: f32 = 0.1;
+const ROCK_MIN_SIZE: f32 = 0.05;
+const ROCK_MAX_SIZE: f32 = 0.2;
+
+const PLAYFIELD_BOUNCE_COEFFICIENT: f32 = -0.5; // portion of player's velocity to reflect when they collide with the bottom of the playfield.
+
+const PLAYFIELD_BACKGROUND_COLOR: (f32, f32, f32) = (0.0, 1.0, 0.5);
+const PLAYFIELD_BACKGROUND_DEPTH: f32 = 0.6;
+const PLAYFIELD_BACKGROUND_POSITION: (f32, f32) = (0.0, 0.0);
+const PLAYFIELD_BACKGROUND_SIZE: f32 = 1.0;
+
+const PLAYFIELD_SCORE_ZONE_DEPTH: f32 = 0.4;
+
+const WINDOW_INITIAL_WIDTH: u32 = 1024;
+const WINDOW_INITIAL_HEIGHT: u32 = 768;
+
+// const PLAYFIELD_SCORE_ZONE_LT_COLOR: (f32, f32, f32) = (0.0, 0.0, 0.0);
+// const PLAYFIELD_SCORE_ZONE_LT_POSITION: (f32, f32) = (-0.75, 0.75);
+// const PLAYFIELD_SCORE_ZONE_LT_SIZE: f32 = 0.25;
+
+const PLAYFIELD_SCORE_ZONE_RT_COLOR: (f32, f32, f32) = (0.5, 1.0, 0.0);
+const PLAYFIELD_SCORE_ZONE_RT_POSITION: (f32, f32) = (0.75, 0.75);
+const PLAYFIELD_SCORE_ZONE_RT_SIZE: f32 = 0.25;
+
+// const PLAYFIELD_SCORE_ZONE_RB_COLOR: (f32, f32, f32) = (0.0, 0.0, 0.0);
+// const PLAYFIELD_SCORE_ZONE_RB_POSITION: (f32, f32) = (0.75, -0.75);
+// const PLAYFIELD_SCORE_ZONE_RB_SIZE: f32 = 0.25;
+
+const PLAYFIELD_SCORE_ZONE_LB_COLOR: (f32, f32, f32) = (1.0, 0.0, 0.5);
+const PLAYFIELD_SCORE_ZONE_LB_POSITION: (f32, f32) = (-0.75, -0.75);
+const PLAYFIELD_SCORE_ZONE_LB_SIZE: f32 = 0.25;
+
 
 #[derive(Copy, Clone)]
 struct Vert {
-    pos: (f32, f32, f32),
+    position: (f32, f32, f32),
     color: (f32, f32, f32),
 }
-glium::implement_vertex!(Vert, pos, color);
+glium::implement_vertex!(Vert, position, color);
 
 #[derive(Copy, Clone)]
 struct PhysObj {
-    pos: (f32, f32),
+    position: (f32, f32),
     velocity: (f32, f32),
     size: f32,
+}
+impl PhysObj {
+    fn position_delta(&mut self, time_delta: f32) {
+	(*self).position.0 += (*self).velocity.0 * time_delta;
+	(*self).position.1 += (*self).velocity.1 * time_delta;
+    }
+}
+
+fn rand_range(min: f32, max: f32) -> f32 {
+    rand::random::<f32>() * (max - min) + min
+}
+
+type Quad = [Vert; 6];
+const fn quad_from_verts(lt: Vert, rt: Vert, rb: Vert, lb: Vert) -> Quad {
+    [
+	rt, lt, lb,
+	rt, lb, rb
+    ]
+}
+fn diamond_from_dims(width: f32, height: f32, depth: f32, center: (f32, f32), color: (f32, f32, f32)) -> Quad {
+    let center_x = center.0;
+    let center_y = center.1;
+    quad_from_verts(
+	Vert {
+	    position: (center_x,         center_y + height, depth),
+	    color: color,
+	},
+	Vert {
+	    position: (center_x + width, center_y,          depth),
+	    color: color,
+	},
+	Vert {
+	    position: (center_x,         center_y - height, depth),
+	    color: color,
+	},
+	Vert {
+	    position: (center_x - width, center_y,          depth),
+	    color: color,
+	}
+    )
+}
+fn square_from_dims(width: f32, height: f32, depth: f32, center: (f32, f32), color: (f32, f32, f32)) -> Quad {
+    let center_x = center.0;
+    let center_y = center.1;
+    quad_from_verts(
+	Vert {
+	    position: (center_x - width, center_y + height, depth),
+	    color: color,
+	},
+	Vert {
+	    position: (center_x + width, center_y + height, depth),
+	    color: color,
+	},
+	Vert {
+	    position: (center_x + width, center_y - height, depth),
+	    color: color,
+	},
+	Vert {
+	    position: (center_x - width, center_y - height, depth),
+	    color: color,
+	}
+    )
+}
+const fn _square_from_edge_positions(left: f32, right: f32, top: f32, bottom: f32, depth: f32, color: (f32, f32, f32)) -> Quad {
+    quad_from_verts(
+	Vert {
+	    position: (left,  top,    depth),
+	    color: color,
+	},
+	Vert {
+	    position: (right, top,    depth),
+	    color: color,
+	},
+	Vert {
+	    position: (right, bottom, depth),
+	    color: color,
+	},
+	Vert {
+	    position: (left,  bottom, depth),
+	    color: color,
+	}
+    )
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // renderer variables
     let mut next_frame_time = std::time::Instant::now();
     let mut then = std::time::Instant::now();
-    let mut win_size_px = (1024, 768);
+
+    let mut frame_times = Vec::new();
 
     // initial setup
     let eve_lp = glium::glutin::event_loop::EventLoop::new();
     let win_b = glium::glutin::window::WindowBuilder::new()
-        .with_inner_size(glium::glutin::dpi::LogicalSize::new(win_size_px.0, win_size_px.1))
+        .with_inner_size(glium::glutin::dpi::LogicalSize::new(WINDOW_INITIAL_WIDTH, WINDOW_INITIAL_HEIGHT))
         .with_title("flap");
     let cntxt_b = glium::glutin::ContextBuilder::new()
 	.with_depth_buffer(24);
@@ -47,9 +174,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut last_rock_spawn_time = None;
     let mut rock_fall_dir = 1.0;
     let mut birdy = PhysObj {
-	pos: (0.0, 0.0),
+	position: (0.0, 0.0),
 	velocity: (0.0, 0.0),
-	size: 0.1,
+	size: BIRDY_SIZE,
     };
 
     let mut rocks = Vec::new();
@@ -58,7 +185,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     eve_lp.run(move |eve, _, ctrl_flow| {
 	// time-wimey stuff
 	let now = std::time::Instant::now();
-	let time_delta = now.duration_since(then);
+	let time_delta = now.duration_since(then).as_secs_f32();
 	next_frame_time += std::time::Duration::from_nanos(16_666_667); // next frame should render 1/60 sec later
 	*ctrl_flow = glium::glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
 
@@ -68,10 +195,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 		event: glium::glutin::event::WindowEvent::CloseRequested,
 		..
 	    } => *ctrl_flow = glium::glutin::event_loop::ControlFlow::ExitWithCode(0),
-	    glium::glutin::event::Event::WindowEvent {
-		event: glium::glutin::event::WindowEvent::Resized(win_size),
-		..
-	    } => glium::glutin::dpi::PhysicalSize{width: win_size_px.0, height: win_size_px.1} = win_size,
 	    glium::glutin::event::Event::WindowEvent {
 		event: glium::glutin::event::WindowEvent::KeyboardInput {
 		    input: glium::glutin::event::KeyboardInput {
@@ -87,12 +210,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 		glium::glutin::event::VirtualKeyCode::Space => if match last_jump_time {
 		    None => std::time::Duration::MAX,
 		    Some(time) => now.duration_since(time)
-		} > JUMP_COOLDOWN {
+		} > BIRDY_JUMP_COOLDOWN {
 		    last_jump_time = Some(now);
-		    birdy.velocity.1 = 0.8;
+		    birdy.velocity.1 = BIRDY_ACCEL_JUMP;
 		},
-		glium::glutin::event::VirtualKeyCode::Left => birdy.velocity.0 = -0.6,
-		glium::glutin::event::VirtualKeyCode::Right => birdy.velocity.0 = 0.6,
+		glium::glutin::event::VirtualKeyCode::Left => birdy.velocity.0 = BIRDY_ACCEL_MOVE * -1.0,
+		glium::glutin::event::VirtualKeyCode::Right => birdy.velocity.0 = BIRDY_ACCEL_MOVE,
 		_ => (),
 	    },
 	    _ => (),
@@ -101,27 +224,26 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 	// logic
 
 	// player stuff
-	birdy.pos.0 += birdy.velocity.0 * time_delta.as_secs_f32();
-	if birdy.pos.0 - birdy.size < -1.0 {
-	    birdy.pos.0 = -1.0 + birdy.size;
+	birdy.position_delta(time_delta);
+	if birdy.position.0 - birdy.size < -1.0 {
+	    birdy.position.0 = -1.0 + birdy.size;
 	}
-	if birdy.pos.0 + birdy.size > 1.0 {
-	    birdy.pos.0 = 1.0 - birdy.size;
+	if birdy.position.0 + birdy.size > 1.0 {
+	    birdy.position.0 = 1.0 - birdy.size;
 	}
-	birdy.pos.1 += birdy.velocity.1 * time_delta.as_secs_f32();
-	if birdy.pos.1 - birdy.size < -1.0 {
-	    birdy.pos.1 = -1.0 + birdy.size;
-	    birdy.velocity.1 *= -0.5 // bounce
+	if birdy.position.1 - birdy.size < -1.0 {
+	    birdy.position.1 = -1.0 + birdy.size;
+	    birdy.velocity.1 *= PLAYFIELD_BOUNCE_COEFFICIENT; // bounce
 	}
-	if birdy.pos.1 + birdy.size > 1.0 {
-	    birdy.pos.1 = 1.0 - birdy.size;
+	if birdy.position.1 + birdy.size > 1.0 {
+	    birdy.position.1 = 1.0 - birdy.size;
 	}
-	birdy.velocity.1 += ACCEL_GRAV * time_delta.as_secs_f32();
-	if birdy.velocity.1 < TERM_VEL {
-	    birdy.velocity.1 = TERM_VEL;
+	birdy.velocity.1 += BIRDY_ACCEL_GRAV * time_delta;
+	if birdy.velocity.1 < BIRDY_TERMINAL_VELOCITY {
+	    birdy.velocity.1 = BIRDY_TERMINAL_VELOCITY;
 	}
 	let tmp = birdy.velocity.0;
-	birdy.velocity.0 -= (0.4 * time_delta.as_secs_f32()) * f32::signum(birdy.velocity.0);
+	birdy.velocity.0 += (BIRDY_DECCEL_MOVE * time_delta) * f32::signum(birdy.velocity.0);
 	if tmp.is_sign_positive() != birdy.velocity.0.is_sign_positive() {
 	    birdy.velocity.0 = 0.0;
 	}
@@ -132,27 +254,26 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 	    Some(time) => now.duration_since(time)
 	} > ROCK_COOLDOWN {
 	    last_rock_spawn_time = Some(now);
-	    let size = rand::random::<f32>() * 0.025 + 0.175;
+	    let size = rand_range(ROCK_MIN_SIZE, ROCK_MAX_SIZE);
 	    let mut x = rand::random::<f32>() * (1.0 - size);
 	    if rand::random() {
 		x *= -1.0;
 	    }
 	    rocks.push(
 		PhysObj {
-		    pos: (x, 1.5 * rock_fall_dir),
-		    velocity: (0.0, rock_fall_dir * (rand::random::<f32>() * -0.5 - 0.5)),
+		    position: (x, ROCK_SPAWN_DIST * rock_fall_dir),
+		    velocity: (0.0, rock_fall_dir * -1.0 * rand_range(ROCK_MIN_VELOCITY, ROCK_MAX_VELOCITY)),
 		    size: size,
 		}
 	    );
 	    rock_fall_dir *= -1.0;
 	}
-	for mut rock in rocks.iter_mut() {
-	    rock.pos.0 += rock.velocity.0 * time_delta.as_secs_f32();
-	    rock.pos.1 += rock.velocity.1 * time_delta.as_secs_f32();
+	for rock in rocks.iter_mut() {
+	    rock.position_delta(time_delta);
 	}
 	let mut i = 0;
 	while i < rocks.len() {
-	    if f32::abs(rocks[i].pos.1) > 2.0 {
+	    if f32::abs(rocks[i].position.1) > ROCK_DESPAWN_DIST {
 		rocks.remove(i);
 	    }
 	    else {
@@ -161,157 +282,54 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 	}
 
 	// get all our vertices together
-	let mut vertices = vec![ // bird
-	    Vert {
-		pos: (0.1, 0.0, 0.0),
-		color: (1.0, 0.9, 0.0),
-	    },
-	    Vert {
-		pos: (0.0, 0.1, 0.0),
-		color: (1.0, 0.9, 0.0),
-	    },
-	    Vert {
-		pos: (-0.1, 0.0, 0.0),
-		color: (1.0, 0.9, 0.0),
-	    },
-	    Vert {
-		pos: (0.1, 0.0, 0.0),
-		color: (1.0, 0.9, 0.0),
-	    },
-	    Vert {
-		pos: (-0.1, 0.0, 0.0),
-		color: (1.0, 0.9, 0.0),
-	    },
-	    Vert {
-		pos: (0.0, -0.1, 0.0),
-		color: (1.0, 0.9, 0.0),
-	    },
-	];
-	for mut vrtx in vertices.iter_mut() {
-	    (*vrtx).pos.0 += birdy.pos.0;
-	    (*vrtx).pos.1 += birdy.pos.1;
-	}
-
-	for mut rock in rocks.iter_mut() {
-	    vertices.extend_from_slice( // playfield
-		&[
-		    Vert {
-			pos: (rock.size + rock.pos.0, 0.0 + rock.pos.1, 0.0),
-			color: (0.5, 0.5, 0.6),
-		    },
-		    Vert {
-			pos: (0.0 + rock.pos.0, rock.size + rock.pos.1, 0.0),
-			color: (0.5, 0.5, 0.6),
-		    },
-		    Vert {
-			pos: (-1.0 * rock.size + rock.pos.0, 0.0 + rock.pos.1, 0.0),
-			color: (0.5, 0.5, 0.6),
-		    },
-		    Vert {
-			pos: (rock.size + rock.pos.0, 0.0 + rock.pos.1, 0.0),
-			color: (0.5, 0.5, 0.6),
-		    },
-		    Vert {
-			pos: (-1.0 * rock.size + rock.pos.0, 0.0 + rock.pos.1, 0.0),
-			color: (0.5, 0.5, 0.6),
-		    },
-		    Vert {
-			pos: (0.0 + rock.pos.0, -1.0 * rock.size + rock.pos.1, 0.0),
-			color: (0.5, 0.5, 0.6),
-		    },
-		]
+	let mut vertices = Vec::new();
+	vertices.extend_from_slice(
+	    &diamond_from_dims(birdy.size, birdy.size, BIRDY_DEPTH, birdy.position, BIRDY_COLOR)
+	);
+	for rock in rocks.iter() { // rocks, duh
+	    vertices.extend_from_slice(
+		&diamond_from_dims(rock.size, rock.size, ROCK_DEPTH, rock.position, ROCK_COLOR)
 	    );
 	}
-
-	vertices.extend_from_slice( // playfield
-	    &[
-		// Background
-		Vert {
-		    pos: (1.0, 1.0, 0.5),
-		    color: (0.0, 1.0, 0.5),
-		},
-		Vert {
-		    pos: (-1.0, 1.0, 0.5),
-		    color: (0.0, 1.0, 0.5),
-		},
-		Vert {
-		    pos: (-1.0, -1.0, 0.5),
-		    color: (0.0, 1.0, 0.5),
-		},
-		Vert {
-		    pos: (1.0, 1.0, 0.5),
-		    color: (0.0, 1.0, 0.5),
-		},
-		Vert {
-		    pos: (-1.0, -1.0, 0.5),
-		    color: (0.0, 1.0, 0.5),
-		},
-		Vert {
-		    pos: (1.0, -1.0, 0.5),
-		    color: (0.0, 1.0, 0.5),
-		},
-		// left side
-		Vert {
-		    pos: (-0.5, -0.5, 0.4),
-		    color: (1.0, 0.0, 0.5),
-		},
-		Vert {
-		    pos: (-1.0, -0.5, 0.4),
-		    color: (1.0, 0.0, 0.5),
-		},
-		Vert {
-		    pos: (-1.0, -1.0, 0.4),
-		    color: (1.0, 0.0, 0.5),
-		},
-		Vert {
-		    pos: (-0.5, -0.5, 0.4),
-		    color: (1.0, 0.0, 0.5),
-		},
-		Vert {
-		    pos: (-1.0, -1.0, 0.4),
-		    color: (1.0, 0.0, 0.5),
-		},
-		Vert {
-		    pos: (-0.5, -1.0, 0.4),
-		    color: (1.0, 0.0, 0.5),
-		},
-		// right side
-		Vert {
-		    pos: (1.0, 1.0, 0.4),
-		    color: (0.5, 1.0, 0.0),
-		},
-		Vert {
-		    pos: (0.5, 1.0, 0.4),
-		    color: (0.5, 1.0, 0.0),
-		},
-		Vert {
-		    pos: (0.5, 0.5, 0.4),
-		    color: (0.5, 1.0, 0.0),
-		},
-		Vert {
-		    pos: (1.0, 1.0, 0.4),
-		    color: (0.5, 1.0, 0.0),
-		},
-		Vert {
-		    pos: (0.5, 0.5, 0.4),
-		    color: (0.5, 1.0, 0.0),
-		},
-		Vert {
-		    pos: (1.0, 0.5, 0.4),
-		    color: (0.5, 1.0, 0.0),
-		},
-	    ]
+	// playfield
+	vertices.extend_from_slice( // background
+	    &square_from_dims(
+		PLAYFIELD_BACKGROUND_SIZE,
+		PLAYFIELD_BACKGROUND_SIZE,
+		PLAYFIELD_BACKGROUND_DEPTH,
+		PLAYFIELD_BACKGROUND_POSITION,
+		PLAYFIELD_BACKGROUND_COLOR
+	    )
+	);
+	vertices.extend_from_slice( // left-bottom
+	    &square_from_dims(
+		PLAYFIELD_SCORE_ZONE_LB_SIZE,
+		PLAYFIELD_SCORE_ZONE_LB_SIZE,
+		PLAYFIELD_SCORE_ZONE_DEPTH,
+		PLAYFIELD_SCORE_ZONE_LB_POSITION,
+		PLAYFIELD_SCORE_ZONE_LB_COLOR
+	    )
+	);
+	vertices.extend_from_slice( // right-top
+	    &square_from_dims(
+		PLAYFIELD_SCORE_ZONE_RT_SIZE,
+		PLAYFIELD_SCORE_ZONE_RT_SIZE,
+		PLAYFIELD_SCORE_ZONE_DEPTH,
+		PLAYFIELD_SCORE_ZONE_RT_POSITION,
+		PLAYFIELD_SCORE_ZONE_RT_COLOR
+	    )
 	);
 
-	// scaling
-	if win_size_px.0 > win_size_px.1 {
+	// scale playfield to square
+	let (window_width, window_height) = disp.get_framebuffer_dimensions();
+	if window_width > window_height {
 	    for mut vrtx in vertices.iter_mut() {
-		(*vrtx).pos.0 *= win_size_px.1 as f32 / win_size_px.0 as f32;
+		(*vrtx).position.0 *= window_height as f32 / window_width as f32;
 	    }
 	}
-	if win_size_px.0 < win_size_px.1 {
+	if window_width < window_height {
 	    for mut vrtx in vertices.iter_mut() {
-		(*vrtx).pos.1 *= win_size_px.0 as f32 / win_size_px.1 as f32;
+		(*vrtx).position.1 *= window_width as f32 / window_height as f32;
 	    }
 	}
 
@@ -324,8 +342,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 	    Some(f32::MAX), // depth
 	    None // stencil
 	);
-	f_buff.draw( // draw birdy to framebuffer
-	    &glium::VertexBuffer::new( // vertices
+	f_buff.draw( // draw vertices to framebuffer
+	    &glium::VertexBuffer::new(
 		&disp,
 		&vertices,
 	    ).unwrap(),
@@ -347,5 +365,15 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
 	// more timey-wimey
 	then = now;
+	frame_times.push(now.elapsed());
+	if frame_times.len() == 60 {
+	    let mut avg_frame_time = std::time::Duration::ZERO;
+	    for frame_time in frame_times.iter() {
+		avg_frame_time += *frame_time;
+	    }
+	    avg_frame_time /= 60;
+	    println!("{:?}", avg_frame_time);
+	    frame_times = Vec::new();
+	}
     });
 }
